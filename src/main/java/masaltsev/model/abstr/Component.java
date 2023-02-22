eeb3d530ca3bcc76dd49d17a1786256ce15d3beb
masaltsev <a.o.masaltsev@gmail.com>
2023-02-22 18:16:02 +0200

    models

---
+++ src/main/java/masaltsev/model/abstr/Component.java
@@ -0,0 +1,33 @@
+package masaltsev.model.abstr;
+
+import java.math.BigDecimal;
+
+public abstract class Component {
+    private String brand;
+    private String model;
+    private BigDecimal price;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
