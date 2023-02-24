72d5b9ec4e9b69799e22bb8c05b15bd00d597390
masaltsev <a.o.masaltsev@gmail.com>
2023-02-24 23:56:24 +0200

    adding to Set optimized, added dispayList

--- src/main/java/masaltsev/util/ChatBot.java
+++ src/main/java/masaltsev/util/ChatBot.java
@@ -15,22 +15,27 @@ public class ChatBot {
     public static String askNameAndSayHello() {
         System.out.println("\nWhat is your name?");
         String name = ConsoleReader.readConsole();
-        System.out.println("\nHello, " + name + "!\n");
+        System.out.println("\nHello, " + name + "!");
         return name;
     }
 
     public static String askComponentId(String componentType, List<String> componentList) {
+        displayList(componentList);
+        System.out.print("\nChoose id between 1 and " + componentList.size() + " of your "
+                + componentType.toUpperCase() + " or 0 to ignore a component: ");
+        return ConsoleReader.readConsole();
+    }
+
+    public static void displayList(List<String> componentList) {
+        System.out.println();
         IntStream.range(0, componentList.size())
                 .peek(i -> System.out.print(i + 1 + ": "))
                 .mapToObj(componentList::get)
                 .forEach(System.out::println);
-        System.out.print("\nChoose id between 1 and " + componentList.size() + " of your "
-                + componentType.toUpperCase() + ": ");
-        return ConsoleReader.readConsole();
     }
 
-    public static void showPrice(BigDecimal price) {
-        System.out.println("\nTotal price is: " + price + "\nGood bye!");
+    public static void showPriceAndSayBye(BigDecimal price) {
+        System.out.println("\nTotal price of your computer is: " + price + "\nGood bye!");
         ConsoleReader.close();
     }
 
